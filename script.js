7120fc253f9e130db9403c3b85da89ab7ca3e38d
Naimcoder <isteiaknaim12@gmail.com>
2022-09-15 01:16:35 +0600

    update css file

--- script.js
+++ script.js
@@ -24,10 +24,14 @@ const typeController = (e) => {
   const newLetter = e.key;
 
   // Handle backspace press
-  if (newLetter == "Backspace") {
-    userText = userText.slice(0, userText.length - 1);
-    return display.removeChild(display.lastChild);
-  }
+ if (newLetter == "Backspace") {
+   userText = userText.slice(0, userText.length - 1);
+   if (display.children[0]) {
+     return display.removeChild(display.lastChild)
+   } else {
+     return;
+   }
+ }
 
   // these are the valid character we are allowing to type
   const validLetters =
